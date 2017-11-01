f1ea8a1bcaf2fffb7ddcd692e02f10fd6203116a
Johannes Brännström <jbrannst@redhat.com>
2017-11-01 10:19:52 +0100

    use upstream launcher

--- create_rhoar.sh
+++ create_rhoar.sh
@@ -6,4 +6,6 @@ export MINISHIFT_PASSWORD=<rhn pasword>
 export MINISHIFT_USERNAME=<rhn user>
 minishift start --vm-driver virtualbox --memory 8196
 oc delete project myproject
+curl -O https://raw.githubusercontent.com/openshiftio/appdev-documentation/master/scripts/deploy_launchpad_mission.sh
+chmod +x deploy_launchpad_mission.sh
 ./deploy_launchpad_mission.sh -p rhoar -i developer:developer -g $GIT_USER:$GIT_TOKEN
